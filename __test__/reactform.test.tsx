9d692ae8a1d0188a5c75a389cbfb2d40be3da744
Jeco99 <jerriccojohncabunagan00@gmail.com>
2024-04-05 20:23:31 +0800

    test: add test to foodlist and reactform

---
+++ __test__/reactform.test.tsx
@@ -0,0 +1,111 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import AddForm from "@/app/addFoodData/page";
+import { mockLocalStorage } from "../utils/test-helper/mockLocalStorage";
+
+const { setItemMock } = mockLocalStorage();
+
+describe("AddForm component", () => {
+  it("should render the basic fields", () => {
+    render(<AddForm />);
+    expect(
+      screen.getByRole("heading", { name: "Add New Food Data" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: "Name" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: "Description" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("spinbutton", { name: "Rating" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: "URL" })).toBeInTheDocument();
+  });
+
+  it("submits the form with valid data", async () => {
+    render(<AddForm />);
+
+    fireEvent.change(screen.getByLabelText("Name"), {
+      target: { value: "Sushi" },
+    });
+    fireEvent.change(screen.getByLabelText("Description"), {
+      target: { value: "Test Description" },
+    });
+    fireEvent.change(screen.getByLabelText("URL"), {
+      target: {
+        value:
+          "https://img.freepik.com/free-vector/asia-food-icon-set_1284-4192.jpg?w=996",
+      },
+    });
+    fireEvent.change(screen.getByLabelText("Rating"), {
+      target: { value: 4 },
+    });
+
+    fireEvent.click(screen.getByText("Submit"));
+
+    const expectedData = JSON.parse(
+      JSON.stringify({
+        rating: 4,
+        image_url:
+          "https://img.freepik.com/free-vector/asia-food-icon-set_1284-4192.jpg?w=996",
+
+        description: "Test Description",
+        name: "Sushi",
+      })
+    );
+
+    await waitFor(() => {
+      expect(setItemMock).toHaveBeenCalledWith(
+        "newData",
+        JSON.stringify(expectedData)
+      );
+    });
+    expect(screen.getByText("Data stored successfully")).toBeInTheDocument();
+  });
+
+  it("shows error messages for invalid data", async () => {
+    render(<AddForm />);
+
+    fireEvent.click(screen.getByText("Submit"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Name is required")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Description is required")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("URL is required")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.getByText("Rating must be a number")).toBeInTheDocument();
+    });
+  });
+
+  it("checks minimum input values for the rating field", async () => {
+    render(<AddForm />);
+
+    fireEvent.change(screen.getByLabelText("Rating"), {
+      target: { value: 0 },
+    });
+
+    fireEvent.click(screen.getByText("Submit"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Rating must be at least 1")).toBeInTheDocument();
+    });
+  });
+
+  it("checks maximum input values for the rating field", async () => {
+    render(<AddForm />);
+
+    fireEvent.change(screen.getByLabelText("Rating"), {
+      target: { value: 6 },
+    });
+
+    fireEvent.click(screen.getByText("Submit"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Rating must be at most 5")).toBeInTheDocument();
+    });
+  });
+});
